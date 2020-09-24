4e307bc9960701cc0a4f6574ac57699f63962a0a
edidonat <edidonat@nd.edu>
2020-09-24 15:24:49 -0400

    agh

--- uniqueWages.sh
+++ uniqueWages.sh
@@ -1,3 +1,2 @@
 # Task 1: Create file with unique gender-yearsExperience pairs sorted
-cat wages.csv | cut -d , -f 1,2 | sort -k 1 -n | uniq -d | sort -k 2 -n >> sortedWages.txt
-
+cat wages.csv | cut -d , -f 1,2 | sort -k 1,2 -V | uniq -d | tr "," " " >> sortedWages.txt
