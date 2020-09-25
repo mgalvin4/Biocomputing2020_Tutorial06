de0aa67cb74278ffbd6323413377c5e74f52d2ad
Maddie Galvin <mgalvin4@nd.edu>
2020-09-25 09:57:21 -0400

    Final edits

--- testWages.sh
+++ testWages.sh
@@ -1,5 +1,5 @@
 #Task 1: Create file with unique gender-yearsExperience combinations sorted
-cat wages.csv | cut -d , -f 1,2 | sort -k 1,2 -V | uniq -d | tr "," " " >> testSort.txt
+cat wages.csv | cut -d , -f 1,2 | sort -k 1,2 -V | uniq -d | tr "," " " >> sortedWages.txt
 #
 #Task 2: Return highest earner, lowest earner, and number of females in top ten earners
 echo "highest earner: "
@@ -11,6 +11,6 @@ cat wages.csv | sort -n -k 3 | head -n 11 | grep -c "female"
 #
 #Task 3: Effect of graduating college on minimum wage
 echo "Effect of Graduating"
-highschool=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep "12" | sort -n | head -n 1 | cut -d , -f 2)
-college=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep "16" | sort -n | head -n 1 | cut -d , -f 2)
-echo "$college - $highschool"
+highschool=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep "12" | sort -n | head -n 1 | cut -d , -f 4)
+college=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep "16" | sort -n | head -n 1 | cut -d , -f 4)
+echo "$college - $highschool" | bc
