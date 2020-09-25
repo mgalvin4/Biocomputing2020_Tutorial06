e57043d3c6925af009875699006df43cde2ed81f
Maddie Galvin <mgalvin4@nd.edu>
2020-09-24 22:51:19 -0400

    Task 3

--- testWages.sh
+++ testWages.sh
@@ -5,3 +5,8 @@ cat wages.csv | cut -d , -f 1,2 | sort -k 1,2 -V | uniq -d | tr "," " " >> testS
 echo "highest earner: " | cat wages.csv | cut -d , -f 1,2,4 |  sort -n -r -k 4 | head -n 1
 echo "lowest earner: " | cat wages.csv | cut -d , -f 1,2,4 | sort -n -k 4 | head -n 1
 echo "number of females in top 10 earners: " | cat wages.csv | sort -n -k 3 | head -n 11 | grep -c "female"
+
+#Task 3
+highschool=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep "12" | sort -n | head -n 1 | cut -d , -f 2)
+college=$(cat wages.csv | cut -d , -f 3,4 | tr "," " " | grep "16" | sort -n | head -n 1 | cut -d , -f 2)
+echo "$college - $highschool" | bc
